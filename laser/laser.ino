5f3f349203a5a1a064d0055706134386e3dd41ed
Janik-ux <mianikproductions@gmail.com>
2023-05-31 14:23:56 +0200

    Added sleep mode, switched to esp32-wroom-32

--- laser/laser.ino
+++ laser/laser.ino
@@ -6,6 +6,12 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  5
+#define ONBOARD_LED  2
+
+RTC_DATA_ATTR int bootCount = 0;
+
 const char* ssid = WIFI_SSID;
 const char* password = PASSWORD;
 
@@ -13,14 +19,21 @@ Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin(2, 14);
+
+  // blink led to indicate new boot
+  pinMode(ONBOARD_LED,OUTPUT);
+  digitalWrite(ONBOARD_LED,HIGH);
+  delay(100);
+  digitalWrite(ONBOARD_LED,LOW);
 
   // wait until serial port opens for native USB devices
   while (! Serial) {
     delay(1);
   }
-  
-  Serial.println("waterlevel test with VL53L0X");
+
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+  Serial.println("Waterlevel test with VL53L0X");
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -30,33 +43,33 @@ void setup() {
     ESP.restart();
   }
 
-  ArduinoOTA
-    .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
+  // ArduinoOTA
+  //   .onStart([]() {
+  //     String type;
+  //     if (ArduinoOTA.getCommand() == U_FLASH)
+  //       type = "sketch";
+  //     else // U_SPIFFS
+  //       type = "filesystem";
  
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
+  //     // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+  //     Serial.println("Start updating " + type);
+  //   })
+  //   .onEnd([]() {
+  //     Serial.println("\nEnd");
+  //   })
+  //   .onProgress([](unsigned int progress, unsigned int total) {
+  //     Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  //   })
+  //   .onError([](ota_error_t error) {
+  //     Serial.printf("Error[%u]: ", error);
+  //     if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+  //     else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+  //     else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+  //     else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+  //     else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  //   });
  
-  ArduinoOTA.begin();
+  // ArduinoOTA.begin();
  
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
@@ -65,23 +78,26 @@ void setup() {
     Serial.println(F("Failed to boot VL53L0X"));
     while(1);
   }
-  // power 
-  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+
+  // measuring the data
+  VL53L0X_RangingMeasurementData_t measure;
+  Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Going to sleep now");
+  delay(1000);
+  Serial.flush(); 
+  esp_deep_sleep_start();
 }
 
 
 void loop() {
-  ArduinoOTA.handle();
-//  VL53L0X_RangingMeasurementData_t measure;
-//    
-//  Serial.print("Reading a measurement... ");
-//  lox.rangingTest(&measure, true); // pass in 'true' to get debug data printout!
-//
-//  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-//    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
-//  } else {
-//    Serial.println(" out of range ");
-//  }
-//    
-//  delay(100);
+
 }
